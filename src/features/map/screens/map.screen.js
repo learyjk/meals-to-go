8178b036d85cd3f7c7097bfece6341cdd3adb386
Keegan <leary.keegan@gmail.com>
2022-07-08 21:33:00 -0700

    sets map region

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 import { Search } from "../components/search.components";
 
@@ -9,9 +11,35 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => (
-  <>
-    <Search />
-    <MapView style={{ height: "100%" }} />
-  </>
-);
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  const { lat, lng, viewport } = location;
+
+  const [latDelta, setLatDelta] = useState(0);
+  console.log(viewport);
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}>
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
+      {/* <MapView style={{ height: "100%" }} /> */}
+    </>
+  );
+};
