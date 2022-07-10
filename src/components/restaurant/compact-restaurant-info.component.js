75ae2a547757c71b9305f1e7df0bc85e25bf81a0
Keegan <leary.keegan@gmail.com>
2022-07-10 14:14:41 -0700

    adding compact view

---
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text } from "react-native";
+
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const Item = styled.View`
+  padding: 10px;
+  max-width: 120px;
+  align-items: center;
+`;
+
+export const CompactRestaurantInfo = ({ restaurant }) => {
+  return (
+    <Item>
+      <CompactImage src={{ uri: restaurant.photos[0] }} />
+      <Text>{restaurant.name}</Text>
+    </Item>
+  );
+};
--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Text } from "react-native";
 import styled from "styled-components/native";
 import { restaurantsRequest } from "../../../services/restaurants/restaurants.service";
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 
 // const myText = styled.Text``;
 
 const MapCallout = ({ restaurant }) => {
-  return <Text>{restaurant.name}</Text>;
+  return <CompactRestaurantInfo restaurant={restaurant} />;
 };
 
 export default MapCallout;
